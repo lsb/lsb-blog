293ca6632c5846c5d6219ea153ca08a28bb0680f
lsb <leebutterman@gmail.com>
2013-09-11 08:40:32 +0000

    force gzip

--- make-and-upload.sh
+++ make-and-upload.sh
@@ -1,2 +1,7 @@
 ~/blog.sh/make-all.sh
-s3cmd sync slightlynew s3://www.leebutterman.com/
+for f in slightlynew/*
+do
+  gzip -9 $f
+  mv ${f}.gz $f
+done
+s3cmd sync --add-header='Content-Encoding: gzip' slightlynew s3://www.leebutterman.com/
