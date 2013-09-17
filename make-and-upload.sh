dff2dee14a5bb802991720994b82a65a1b2af5f2
lsb <leebutterman@gmail.com>
2013-09-17 14:10:57 +0000

    upload anything

--- make-and-upload.sh
+++ make-and-upload.sh
@@ -1,7 +1,7 @@
 ~/blog.sh/make-all.sh
-for f in slightlynew/*
+for f in $1/*
 do
   gzip -9 $f
   mv ${f}.gz $f
 done
-s3cmd sync --add-header='Content-Encoding: gzip' slightlynew s3://www.leebutterman.com/
+s3cmd sync --add-header='Content-Encoding: gzip' $1 s3://www.leebutterman.com/
